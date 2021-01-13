12dcead14a54e236f72a57edfe29d90c324d36e5
Alex <kpuk77@bk.ru>
2021-01-13 10:39:17 +0800

    Добавьте файлы проекта.

---
+++ AsteroidGame/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidGame
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form game_form = new Form();
+
+            game_form.Width = 800;
+            game_form.Height = 600;
+            game_form.Text = "Asteroid game";
+            game_form.MinimizeBox = false;
+            game_form.MaximizeBox = false;
+            game_form.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            game_form.Show();
+            Application.Run(game_form);
+        }
+    }
+}

8afcd07604e1f94e5b78de1479c30c26c3c3fa5a
Alex <kpuk77@bk.ru>
2021-01-26 23:48:15 +0800

    Частотный массив => LinQ

---
+++ LoveLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LoveLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var intList = Enumerable
+                .Range(1, 200)
+                .Select(i => rand.Next(1, 11)).ToList();
+
+            var freqDictionary = intList
+                .GroupBy(i => i)
+                //.OrderBy(i => i.Key)
+                .ToDictionary(i => i.Key, i => i.Count());
+        }
+    }
+}
